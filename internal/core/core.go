8ab144b815509acd9e0b93551df4acf58f741c66
Allen <allenvs@mail.ru>
2023-03-05 20:03:18 +0600

    feat: started buidling play method

--- internal/core/core.go
+++ internal/core/core.go
@@ -17,3 +17,15 @@ type playlist struct {
 	name      string
 	firstSong *track
 }
+
+// [ ] Play - starts playing / начинает воспроизведение
+func (pl *playlist) play() error {
+	t := time.NewTimer()
+	return nil
+}
+
+// TODO:
+// [ ] Pause - приостанавливает воспроизведение
+// [ ] AddSong - добавляет в конец плейлиста песню
+// [ ] Next воспроизвести след песню
+// [ ] Prev воспроизвести предыдущую песню
